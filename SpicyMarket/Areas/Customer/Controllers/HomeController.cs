2c11f793dacab5fcceccdb274cd58cba0852f1cf
Ahassnat <alhassnatabdallah@gmail.com>
2021-08-31 15:25:13 +0300

    Customer Index View Model Part 1 show the offer picture in carousel

--- SpicyMarket/Areas/Customer/Controllers/HomeController.cs
+++ SpicyMarket/Areas/Customer/Controllers/HomeController.cs
@@ -1,38 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using SpicyMarket.Models;
+using Microsoft.EntityFrameworkCore;
+using SpicyMarket.Data;
+using SpicyMarket.Models.ViewModel;
 
-namespace SpicyMarket.Controllers
+namespace SpicyMarket.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
-
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var indexViewModel = new IndexViewModel()
+            {
+                Categories = await _context.Categories.ToListAsync(),
+                Coupons = await _context.Coupons.Where(x=>x.IsActive == true) // to show the active coupon Only 
+                                                .ToListAsync(), 
+                MenuItems = await _context.MenuItems.Include(x => x.Category)
+                                                    .Include(x => x.SubCategory)
+                                                    .ToListAsync()
+            };
+            return View(indexViewModel);
         }
     }
 }
