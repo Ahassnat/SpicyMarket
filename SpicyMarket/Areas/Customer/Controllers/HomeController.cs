a66b3b95f9a0541501501eb5d63009e7115168f5
Ahassnat <alhassnatabdallah@gmail.com>
2021-09-02 17:12:38 +0300

    Shipping Cart part 2 Authorize the Detalis

--- SpicyMarket/Areas/Customer/Controllers/HomeController.cs
+++ SpicyMarket/Areas/Customer/Controllers/HomeController.cs
@@ -35,7 +35,7 @@ namespace SpicyMarket.Areas.Customer.Controllers
             return View(indexViewModel);
         }
         [HttpGet]
-     //   [Authorize]
+        [Authorize]
         public async Task<IActionResult> Details(int id)
         {
             var menuItem = await _context.MenuItems.Include(x => x.Category)
@@ -52,56 +52,6 @@ namespace SpicyMarket.Areas.Customer.Controllers
             return View(shoppinCart);
         }
 
-        //[HttpPost]
-        //[AutoValidateAntiforgeryToken]
-        //[Authorize]
-        //public async Task<IActionResult> Details(ShoppingCart shoppingCart)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var claimsIdentity = (ClaimsIdentity)User.Identity;
-        //        var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-        //        shoppingCart.ApplicationUserId = claim.Value;
-
-        //        // for number of menu items for detective user  
-        //        var shoppingCartFromDb = await _context.ShoppingCarts
-        //                                                .Where(x =>
-        //                                                   x.ApplicationUserId == shoppingCart.ApplicationUserId &&
-        //                                                   x.MenuItemId == shoppingCart.MenuItemId)
-        //                                                .FirstOrDefaultAsync();
-
-        //        if(shoppingCartFromDb == null) // means => add to count value
-        //        {
-        //            _context.ShoppingCarts.Add(shoppingCartFromDb);
-        //        }
-        //        else // mean => update the count value 
-        //        {
-        //            //shoppingCartFromDb.Count = shoppingCartFromDb.Count + shoppingCart.Count;
-        //            shoppingCartFromDb.Count += shoppingCart.Count;
-        //        }
-
-        //        await _context.SaveChangesAsync();
-        //        var count = _context.ShoppingCarts.Where(x =>
-        //                                                x.ApplicationUserId == shoppingCart.ApplicationUserId)
-        //                                          .ToList().Count;
-        //        return RedirectToAction(nameof(Index));
-
-        //    }
-        //    else
-        //    {
-        //        var menuItem = await _context.MenuItems.Include(x => x.Category)
-        //                                          .Include(x => x.SubCategory)
-        //                                          .Where(x => x.Id == shoppingCart.Id)
-        //                                          .FirstOrDefaultAsync();
-
-        //        var shoppinCartObj = new ShoppingCart()
-        //        {
-        //            MenuItem = menuItem,
-        //            MenuItemId = menuItem.Id
-        //        };
-
-        //        return View(shoppinCartObj);
-        //    }
-        //}
+       
     }
 }
