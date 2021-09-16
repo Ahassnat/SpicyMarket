356674d84c4c89e4afb03de4989b147dbf2852b2
Ahassnat <alhassnatabdallah@gmail.com>
2021-09-16 21:25:06 +0300

    Pagination Part 1 - Create Model For Paging Info - Initilize Customize TagHelper Class - Create New view model for order history list and pagination - modefy the OrderHistory Action method to add pagination customization - Modify the Order Histor View by adding the new viewmodel and add pagination - add this link @addTagHelper SpicyMarket.TagHelpers.*,SpicyMarket to _ViewImports to decleare the custom tag helper

--- SpicyMarket/Areas/Customer/Controllers/OrdersController.cs
+++ SpicyMarket/Areas/Customer/Controllers/OrdersController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SpicyMarket.Data;
+using SpicyMarket.Models;
 using SpicyMarket.Models.ViewModel;
 using SpicyMarket.Utility;
 
@@ -39,13 +40,19 @@ namespace SpicyMarket.Areas.Customer.Controllers
             return View(orderDetalisVM);
         }
 
+        private int pageSize = 2;
+
         [Authorize]
-        public async Task<IActionResult> OrderHistory()
+        public async Task<IActionResult> OrderHistory(int pageNumber = 1)
         {
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
 
-            var orderDetailsVMList = new List<OrderDetailsViewModel>();
+            //var orderDetailsVMList = new List<OrderDetailsViewModel>();
+            var orderListVM = new OrderListViewModel()
+            {
+                Orders = new List<OrderDetailsViewModel>()
+            };
 
             var orderHeadersList = await _context.OrderHeaders.Include(x => x.ApplicationUser)
                                                           .Where(m => m.UserId == claim.Value)
@@ -58,10 +65,24 @@ namespace SpicyMarket.Areas.Customer.Controllers
                     OrderDetails = await _context.OrderDetails.Where(x => x.OrderId == orderHeader.Id)
                                                               .ToListAsync()
                 };
-                orderDetailsVMList.Add(orderDetailsVM);
+                orderListVM.Orders.Add(orderDetailsVM);
             }
+            // Paging Information 
+            var count = orderListVM.Orders.Count; // represent the Total page in PagingInfo Class
+            orderListVM.Orders = orderListVM.Orders.OrderByDescending(x => x.OrderHeader.Id)
+                                                   .Skip((pageNumber - 1) * pageSize)
+                                                   .Take(pageSize)
+                                                   .ToList();
+
+            orderListVM.PagingInfo = new PagingInfo()
+            {
+                CurrentPage = pageNumber,
+                RecordsPerPage = pageSize,
+                TotalRecords = count,
+                UrlParam = "/Customer/Orders/OrderHistory?pageNumber=:"
+            };
            
-            return View(orderDetailsVMList);
+            return View(orderListVM);
         }
         public async Task<IActionResult> GetOrderDetails(int id)
         {
---
+++ SpicyMarket/TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SpicyMarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpicyMarket.TagHelpers
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private readonly IUrlHelperFactory _urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory urlHelperFactory)
+        {
+            _urlHelperFactory = urlHelperFactory;
+        }
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+        public PagingInfo PageModel { get; set; }
+        public string PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; }
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelceted { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+            var result = new TagBuilder("div");
+            for (int i = 1; i <= PageModel.TotalPages; i++)
+            {
+                var tag = new TagBuilder("a");
+                var url = PageModel.UrlParam.Replace(":", i.ToString());
+                tag.Attributes["href"] = url;
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelceted : PageClassNormal);
+                }
+                tag.InnerHtml.Append(i.ToString());
+                result.InnerHtml.AppendHtml(tag);
+            }
+            output.Content.AppendHtml(result.InnerHtml);
+        }
+    }
+
+}
