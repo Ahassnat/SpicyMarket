7308986012babb8aea737ebe52385d68d5b71992
Ahassnat <alhassnatabdallah@gmail.com>
2021-09-20 17:17:48 +0300

    View Componet for UserName section

---
+++ SpicyMarket/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpicyMarket.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SpicyMarket.ViewComponents
+{
+    public class UserNameViewComponent:ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserNameViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var user = claim.Value;
+            var userFromDb = await _context.ApplicationUsers.FindAsync(user);
+            return View(userFromDb);
+        }
+    }
+}
